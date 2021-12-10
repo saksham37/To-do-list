0ccffb979f405df37cc717c00229d401da75ea74
saksham37 <sharma.ibn7@gmail.com>
2021-12-10 23:18:13 +0530

    work on front-end

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,7 +3,7 @@ console.log("working fine");
 const toggleButton = document.querySelector('.toggleButton')
 const plusIcon = document.querySelector('.fa-plus');
 const inputSection  = document.querySelector('#inputSection');
-
+const inputSectionHeight = inputSection.getBoundingClientRect().height;
 
 toggleButton.addEventListener('click',()=>{
     console.log('clicked');
@@ -17,15 +17,25 @@ toggleButton.addEventListener('click',()=>{
        if(inputSection.classList.contains('animate-enter')){
            inputSection.classList.remove('animate-enter');
            inputSection.classList.add('animate-exit');
+
        }
        else{
            inputSection.classList.remove('animate-exit');
            inputSection.classList.add('animate-enter');
         
        }
-     
+
+       console.log("Height of the input section now : " + inputSection.getBoundingClientRect().height);
+       console.log("Top of the input section now : " + inputSection.getBoundingClientRect().top);
+
+      
     // inputSection.style.display = "flex";
 
 
     
 });
+
+//Setting up the lists section
+const listSection = document.querySelector('#lists');
+
+listSection.style.top = inputSection.getBoundingClientRect().top + inputSection.getBoundingClientRect().height;
